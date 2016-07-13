740dd1f2b02d0e7f738f061da39aa7374bc253b5
abryu <wenboyu2208@gmail.com>
2016-07-13 13:04:19 -0400

    Stat

--- FeatureSelectionAndClassifierLearning/getStat.py
+++ FeatureSelectionAndClassifierLearning/getStat.py
@@ -19,3 +19,22 @@ for filePath in pathList:
 for key, value in directoryInfo.iteritems():
     print key, value
 
+# For Naive Bayes
+total4Test = directoryInfo.get("test/student") + directoryInfo.get("test/course") + directoryInfo.get("test/faculty")
+total4Train = directoryInfo.get("train/student") + directoryInfo.get("train/course") + directoryInfo.get("train/faculty")
+
+
+
+prob4TrainStudent = float(directoryInfo.get("train/student"))/float(total4Train)
+prob4TrainCourse = float(directoryInfo.get("train/course"))/float(total4Train)
+prob4TrainFaculty = float(directoryInfo.get("train/faculty"))/float(total4Train)
+
+# prob4TestStudent = int(directoryInfo.get("test/student"))/total4Test
+# prob4TestCourse = int(directoryInfo.get("test/course"))/total4Test
+# prob4TestFaculty = int(directoryInfo.get("test/faculty"))/total4Test
+
+print (total4Test)
+print (total4Train)
+print (prob4TrainStudent)
+print (prob4TrainCourse) 
+print (prob4TrainFaculty)
